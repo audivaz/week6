8b795da879fa7b60a7c62f6f391b096ebadf0be0
audivaz <audivaz3@gmail.com>
2020-10-28 13:04:39 -0400

    update

---
+++ wk6.js
@@ -0,0 +1,122 @@
+
+// 2 - 3
+let string = 'hello';
+string = 'everyone';
+console.log(string);
+
+
+
+
+// 4
+const score = 50;
+console.log(score);
+
+
+//9
+//score = 100;
+
+
+
+// 5 - 6
+
+
+if (true) {
+    var food = 'pizza';
+    
+}
+console.log(food);
+
+
+
+// 7
+
+if (true) {
+    let shark = 'bigfish';
+    console.log(shark);
+}
+//console.log(shark);
+
+
+
+
+
+
+//10
+const bank = [];
+
+
+
+// 11
+bank.push ('fives', 'tens', 'twenties');
+    console.log(bank);
+
+
+
+
+
+// 12
+
+const player1 = {};
+const player2 = {};
+
+
+
+
+
+
+
+
+
+//13 - 14
+player1.name = 'Audi';
+player1.age = 35;
+console.log(`Hello, I am ${player1.name}`);
+
+
+player2.name = 'Bruce-Lee';
+player2.age = 32;
+
+
+
+
+
+
+
+// 15 - 16
+
+function greet() {
+    console.log(`Hello I am ${player2.name} and I am ${player2.age}`);
+}
+
+greet();
+
+
+
+
+
+// 17
+
+
+
+hello = () => {
+    var arrow1 = console.log('hello world');  
+} 
+
+hello();
+
+
+
+
+
+// 18
+
+movie = (str) => {
+    var arrow2 = console.log(`${str}`);
+}
+
+movie('family guy');
+
+
+
+
+// 19
