47cb03cbddb3ed8a691f35163699208e20bca6ee
Harcourt Bull <harcourtbull@Harcourts-iMac.local>
2020-11-03 22:09:53 -0500

    feedback complete

--- hm3.js
+++ hm3.js
@@ -1,55 +1,34 @@
-
-
-
-
-
 // 1 - 2
 
 var var1 = 'string';
 
-
-
 // 3
 var var2 = ['g'];
-    console.log(var2);
-
-
-
-
+console.log(var2);
 
 // 4
 var res = 'o!'.concat(var2);
-    console.log(res);
-
+console.log(res);
 
 // 5
 var var3 = 49;
-    console.log(var3);
-
-
+console.log(var3);
 
 // 6
 var var4 = '65';
-    console.log(var4);
+console.log(var4);
 
 // 7
 
 console.log(var3 + var4);
 
-
-// 8 
+// 8
 
 console.log(parseInt(var3 + var4));
 
-
-
 // 9
 var arr = [];
-    console.log(arr);
-
-
-
-
+console.log(arr);
 
 // 10
 
@@ -57,84 +36,45 @@ var arr = [];
 //     console.log(arr);
 // }
 
-
-
-
-
 arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-    console.log(arr);
-
-
-
-
-
-
+console.log(arr);
 
 // 11
 
 arr.push(11);
-    console.log(arr);
+console.log(arr);
 
-
-
-
-
-// 12 
+// 12
 
 arr.pop();
-    console.log(arr);
-
-
-
-
+console.log(arr);
 
 // 13
 
 arr.unshift(0);
-    console.log(arr);
-
-
-
-
-
-
+console.log(arr);
 
 // 14
 
 arr.shift();
-    console.log(arr);
-
-
-
+console.log(arr);
 
 // 15
 
 arr.splice(0, 1);
-    console.log(arr);
-
-
-
-
-
+console.log(arr);
 
 // 16
 
 arr.splice(7, 2);
-    console.log(arr);
-
+console.log(arr);
 
 // 17
 
-arr.splice(0, 0, -1, 0, 1);   
-    console.log(arr);
-
-
-
+arr.splice(0, 0, -1, 0, 1);
+console.log(arr);
 
 // 18
 
 var copy = arr.slice(0, (0, 3));
-    console.log(copy);
-
-
-
+console.log(copy);
--- hm4.js
+++ hm4.js
@@ -1,192 +1,115 @@
-
-
 // 1
 var str1 = 'hello';
-    console.log(str1);
-
-
-
+console.log(str1);
 
 // 2
-
-
 str1 += ' world';
-    console.log(str1);
-
+console.log(str1);
 
 // 3
-
 var str2 = str1 + '!';
-    console.log(str2);
-
-
-
-
-
+console.log(str2);
 
 // 4
-
 console.log(str2.length);
 
-
-
-
 // 5
-
 console.log(str2.toUpperCase());
 
-
-
-
 // 6
-
 console.log(str2[1]);
 
-
-
-
-
 // 7
-
-
 console.log(str2.indexOf('!'));
 
-
-
-
 // 8
-
-
-var str3 = str2.slice(0, 4)
-
+var str3 = str2.slice(0, 4);
 
 console.log(str3);
 
-
-
-
-
-
-
 // 9
-
-if (str2.includes('ell') === true) {
-    console.log(true);
-};
-
-
-
-
+//? We don't need to check if it equals true, b/c includes() method returns a boolean (true, false) already so we can just simply call the method and will appropriately return true or false depending on if the str included the passed argument 'ell'
+// if (str2.includes('ell') === true) {
+// 	console.log(true);
+// }
+console.log(str2.includes('ell'));
 
 // 10
-
 const me = {
-    name: 'harcourt',
-    age: 26
+	name: 'harcourt',
+	age: 26
 };
 
-
-
-
-
 // 11
-    console.log(me.name);
+console.log(me.name);
 
-    console.log(me.age);
-
-
-
+console.log(me.age);
 
 // 12
 console.log(me['name']);
 
-
-
 // 13
 
-
-me.canDrink = true;
-    console.log(me);
-
-
-
-
+// me.canDrink = true; //? We can improve this by dynamically determining the value of canDrink depending on if age is greater than or equal to 21, we can do this w/ the ternary operator
+me.canDrink = me.age >= 21 ? true : false;
+console.log(me);
 
 // 14
-
-me.greet = function() {
-    console.log(`Hello my name is ${this.name}!`);
-}
-
-
-
-
+//? Great work! Nice use of this!
+me.greet = function () {
+	console.log(`Hello my name is ${this.name}!`);
+};
 
 // 15
-
 me.greet();
 
-
-
-
-
-
-
 // 16
-
-
 // if (me.name.includes('e') === true) {
 //     console.log('Your name has an e');
 // } else if (me.name.includes('e') === false) {
 //     console.log('Your name does not have an E');
 // }
 
-
-
-
-
-
-
-if (me.name.includes('e') === true) {
-    console.log('Your name has an e');
+//? Again we don't have to check it equals true and .includes() returns a boolean
+// if (me.name.includes('e') === true) {
+// 	console.log('Your name has an e');
+// } else {
+// 	console.log('Your name does not have an E');
+// }
+//? Adding in toLowerCase() method so "E" will still match, which I think would best to do.
+if (me.name.toLowerCase().includes('e')) {
+	console.log('Your name has an e');
 } else {
-    console.log('Your name does not have an E');
+	console.log('Your name does not have an e');
 }
 
-
-
-
-
-
-
-
-// 17 
-
-
-
-
-
+// 17
 // console.log()
 
 // if (me.hasOwnProperty(canDrink))
 
+//! Some slight mistakes here, you where checking menubar.canDrink, we don't want to check the menubar object we want to check the me object so we want to check me.canDrink, canDrink is either true or false therefore we don't need to check it's its equal to true just simply pass it in as the conditional statement. Second what is right of the equal side is an anonymous function, which has no name so you want to omit the fun() and replace it with just (). Third you don't want to do the if else if else as you have I would do if (nested if/else) & else as shown below for the desired behavior based on what you are console logging. Also you forgot to call the method at the end. Overall great work though!! It seems like you're doing a lot better w/ the JS stuff I hope the review has been helpful
+// me.thisFun = function fun() {
+// 	if (this.hasOwnProperty('canDrink')) {
+// 		if (menubar.canDrink === true) {
+// 			console.log(`${this.name} can drink`);
+// 		} else if (this.canDrink === false) {
+// 			console.log(`${this.name} cannot drink`);
+// 		} else {
+// 			console.log(`${this.name} hasn't said if he can drink.`);
+// 		}
+// 	}
+// };
+me.thisFun = function () {
+	if (this.hasOwnProperty('canDrink')) {
+		if (this.canDrink) {
+			console.log(`${this.name} can drink`);
+		} else {
+			console.log(`${this.name} cannot drink`);
+		}
+	} else {
+		console.log(`${this.name} has not said if he can drink.`);
+	}
+};
 
-
-
-
-
-
-
-me.thisFun =
-
-function fun () {
-    if (this.hasOwnProperty('canDrink')) {
-        if(menubar.canDrink === true) {
-            console.log(`${this.name} can drink`);
-        } else if (this.canDrink === false) {
-            console.log(`${this.name} cannot drink`);
-        } else {
-            console.log(`${this.name} hasn't said if he can drink.`)
-        }
-    }
-}
+me.thisFun();
